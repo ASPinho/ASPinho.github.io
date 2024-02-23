70d806c66a1f86b595e97c8f5e3cd69f82aeb2c7
André Pinho <andreseguradopinho@gmail.com>
2024-02-23 11:51:37 +0000

    new favicon

--- arkiteq-home/src/index.tsx
+++ arkiteq-home/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from "react-router-dom";
 import './styles/teriyarki-theme.scss';
 import './styles/flex-rules.scss'
 import App from './App';
@@ -8,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
