60aaa036466ed9df4e77de55fe040329bb297c75
André Pinho <andreseguradopinho@gmail.com>
2024-02-26 09:34:07 +0000

    code refactor

--- arkiteq-home/src/index.tsx
+++ arkiteq-home/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from "react-router-dom";
-import './styles/teriyarki-theme.scss';
-import './styles/flex-rules.scss'
+import './styles/teriyarki-theme.css';
+import './styles/flex-rules.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(
