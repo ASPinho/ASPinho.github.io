699a8d3f055176040b6f279fb5bfd6b7a00c6c42
André Pinho <andreseguradopinho@gmail.com>
2024-03-19 18:50:56 +0000

    restyling

--- arkiteq-home/src/routes/ffg.tsx
+++ arkiteq-home/src/routes/ffg.tsx
@@ -4,11 +4,11 @@ import RetroConsole from "../components/retro-console";
 
 function Ffg() {
     return (
-        <>
+        <div id={"announcement-layout"}>
             <RetroConsole>
                 <AnnouncementCard title={"The Friendly Fire Games"} description={"Prepare for battle and meet us @ 15:00 15/04/2024"} dateTime={"2024-04-15T15:00:00"}/>
             </RetroConsole>
-        </>
+        </div>
     )
 }
 
